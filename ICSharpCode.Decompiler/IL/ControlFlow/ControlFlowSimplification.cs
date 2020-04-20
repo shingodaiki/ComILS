icsharpcode/ILSpy

commit c1265ad8303a6cc834778c79d6d31af348966064

Fix #1946: Don't move `return` into `try` block when doing so changes the semantics of a `finally` block.

ICSharpCode.Decompiler/IL/ControlFlow/ControlFlowSimplification.cs
@@ -197,9 +197,22 @@ namespace ICSharpCode.Decompiler.IL.ControlFlow
 		bool IsBranchToReturnBlock(Branch branch)
 		{
 			var targetBlock = branch.TargetBlock;
-			if (targetBlock.Instructions.Count != 1 || targetBlock.FinalInstruction.OpCode != OpCode.Nop)
+			if (targetBlock.Instructions.Count != 1)
 				return false;
-			return targetBlock.Instructions[0].MatchReturn(out var value) && value is LdLoc;
+			if (!targetBlock.Instructions[0].MatchReturn(out var value))
+				return false;
+			if (!targetBlock.MatchLdLoc(out var returnVar))
+				return false;
+			var container = branch.TargetContainer;
+			for (ILInstruction inst = branch; inst != container; inst = inst.Parent) {
+				if (inst.Parent is TryFinally tryFinally && inst.SlotInfo == TryFinally.TryBlockSlot) {
+					// The branch will trigger the finally block.
+					// Moving the return block into the try is only possible if the finally block doesn't touch the return variable.
+					if (returnVar.IsUsedWithin(tryFinally.FinallyBlock))
+						return false;
+				}
+			}
+			return true;
 		}
 		
 		static bool CombineBlockWithNextBlock(BlockContainer container, Block block, ILTransformContext context)

ICSharpCode.Decompiler.Tests/TestCases/Correctness/ControlFlow.cs
@@ -41,6 +41,7 @@ namespace ICSharpCode.Decompiler.Tests.TestCases.Correctness
 			BreakUnlessContinue(true);
 			BreakUnlessContinue(false);
 			TestConditionals();
+			Console.WriteLine("Issue1946:\n" + Issue1946());
 			return 0;
 		}
 
@@ -148,6 +149,19 @@ namespace ICSharpCode.Decompiler.Tests.TestCases.Correctness
 		{
 			byte answer = (byte)(value == 128 ? 255 : 0);
 			return answer;
+		}
+
+		static string Issue1946()
+		{
+			string obj = "1";
+			try {
+				obj = "2";
+			} catch {
+				obj = "3";
+			} finally {
+				obj = "4";
+			}
+			return obj;
 		}
 	}
 }

shingodaiki/ComILS

commit 1ab95129eca57032ee28a0df33a6200a63e00178

#1946: Fix IsBranchToReturnBlock() [was broken in c1265ad830]

ICSharpCode.Decompiler/IL/ControlFlow/ControlFlowSimplification.cs
@@ -201,7 +201,7 @@ namespace ICSharpCode.Decompiler.IL.ControlFlow
 				return false;
 			if (!targetBlock.Instructions[0].MatchReturn(out var value))
 				return false;
-			if (!targetBlock.MatchLdLoc(out var returnVar))
+			if (!value.MatchLdLoc(out var returnVar))
 				return false;
 			var container = branch.TargetContainer;
 			for (ILInstruction inst = branch; inst != container; inst = inst.Parent) {