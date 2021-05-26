icsharpcode/ILSpy

commit 16134e52e47be70461aee0ecd5c01c2dd3c8487c

Fix #2395: Do not use for->foreach transform if collection is not an array.

ICSharpCode.Decompiler/CSharp/Transforms/PatternStatementTransform.cs
@@ -351,6 +351,8 @@ namespace ICSharpCode.Decompiler.CSharp.Transforms
 			var loopContainer = forStatement.Annotation<IL.BlockContainer>();
 			if (itemVariable == null || indexVariable == null || arrayVariable == null)
 				return null;
+			if (arrayVariable.Type.Kind != TypeKind.Array)
+				return null;
 			if (!VariableCanBeUsedAsForeachLocal(itemVariable, forStatement))
 				return null;
 			if (indexVariable.StoreCount != 2 || indexVariable.LoadCount != 3 || indexVariable.AddressCount != 0)
@@ -462,6 +464,8 @@ namespace ICSharpCode.Decompiler.CSharp.Transforms
 				lowerBounds[i] = indexVariable;
 				i++;
 			}
+			if (collection.Type.Kind != TypeKind.Array)
+				return false;
 			var m2 = foreachVariableOnMultArrayAssignPattern.Match(stmt);
 			if (!m2.Success)
 				return false;

ICSharpCode.Decompiler.Tests/TestCases/Pretty/Loops.cs
@@ -269,6 +269,23 @@ namespace ICSharpCode.Decompiler.Tests.TestCases.Pretty
 
 		}
 
+		public class NonEnumerableArrayLike
+		{
+			private readonly int length;
+
+			public Item this[int index] {
+				get {
+					return null;
+				}
+			}
+
+			public int Length {
+				get {
+					return length;
+				}
+			}
+		}
+
 		private IEnumerable<string> alternatives;
 		private object someObject;
 
@@ -466,6 +483,15 @@ namespace ICSharpCode.Decompiler.Tests.TestCases.Pretty
 			foreach (string text in array)
 			{
 				Console.WriteLine(text.ToLower() + text.ToUpper());
+			}
+		}
+
+		public void ForOverNonArray(NonEnumerableArrayLike array)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				Item item = array[i];
+				Console.WriteLine(item.ToString() + item.ToString());
 			}
 		}
 

ICSharpCode.Decompiler.Tests/PrettyTestRunner.cs
@@ -238,7 +238,8 @@ namespace ICSharpCode.Decompiler.Tests
 		{
 			RunForLibrary(cscOptions: cscOptions, decompilerSettings: new DecompilerSettings {
 				// legacy csc generates a dead store in debug builds
-				RemoveDeadStores = (cscOptions == CompilerOptions.None)
+				RemoveDeadStores = (cscOptions == CompilerOptions.None),
+				UseExpressionBodyForCalculatedGetterOnlyProperties = false
 			});
 		}
 

shingodaiki/ComILS

commit 1b1d0367be366c10623875bd0b9cf78efa2fc50e

Fixed regression around foreach-on-string introduced in 16134e52e47be70461aee0ecd5c01c2dd3c8487c

ICSharpCode.Decompiler/CSharp/Transforms/PatternStatementTransform.cs
@@ -348,10 +348,9 @@ namespace ICSharpCode.Decompiler.CSharp.Transforms
 			var itemVariable = m.Get<IdentifierExpression>("itemVariable").Single().GetILVariable();
 			var indexVariable = m.Get<IdentifierExpression>("indexVariable").Single().GetILVariable();
 			var arrayVariable = m.Get<IdentifierExpression>("arrayVariable").Single().GetILVariable();
-			var loopContainer = forStatement.Annotation<IL.BlockContainer>();
 			if (itemVariable == null || indexVariable == null || arrayVariable == null)
 				return null;
-			if (arrayVariable.Type.Kind != TypeKind.Array)
+			if (arrayVariable.Type.Kind != TypeKind.Array && !arrayVariable.Type.IsKnownType(KnownTypeCode.String))
 				return null;
 			if (!VariableCanBeUsedAsForeachLocal(itemVariable, forStatement))
 				return null;

ICSharpCode.Decompiler.Tests/TestCases/Pretty/Loops.cs
@@ -20,6 +20,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace ICSharpCode.Decompiler.Tests.TestCases.Pretty
 {
@@ -611,6 +612,18 @@ namespace ICSharpCode.Decompiler.Tests.TestCases.Pretty
 			for (int i = 0; i < array.Length; i++)
 			{
 				array[i].ToLower();
+			}
+		}
+
+		private static void AppendNamePart(string part, StringBuilder name)
+		{
+			foreach (char c in part)
+			{
+				if (c == '\\')
+				{
+					name.Append('\\');
+				}
+				name.Append(c);
 			}
 		}
 